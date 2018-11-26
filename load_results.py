2abfb8784655bab705751e6e0a3c0eda81f5d55d
Oskari Kerppo <oeoker@utu.fi>
2018-11-26 11:54:24 +0200

    More figures

--- load_results.py
+++ load_results.py
@@ -10,7 +10,7 @@ with open('results.pkl', 'rb') as f:
 
 temps= []
 for x in results:
-	temps.append([x[1][0], x[0][2], x[0][3], x[0][4], x[0][5], x[1][-1], x[1][1][1]])
+	temps.append([x[1][0], x[0][2], x[0][3], x[0][4], x[0][5], x[1][-1], x[1][1][1], x[1][2][1], x[1][3][1]])
 
 print(len(temps))
 #print(temps)
@@ -66,6 +66,8 @@ def create_string_tick(x):
 def create_Z(x, y, l, xi):
 	Z = np.zeros((len(x), len(y)))
 	T = np.zeros((len(x), len(y)))
+	T2 = np.zeros((len(x), len(y)))
+	T3 = np.zeros((len(x), len(y)))
 	M = np.zeros((len(x), len(y)))
 	data = [h for h in temps if h[4] == xi and h[2] == l*h[1]]
 	#print max(data)
@@ -75,17 +77,19 @@ def create_Z(x, y, l, xi):
 			for k in range(len(data)):
 				if data[k][1] == x[i][j] and data[k][3] == y[i][j]:
 					Z[i][j] = data[k][0]
-					if data[k][-2]:
+					if data[k][5]:
 						M[i][j] = 1
 					else:
 						M[i][j] = 0
-					T[i][j] = data[k][-1]
+					T[i][j] = data[k][-3]
+					T2[i][j] = data[k][-2]
+					T3[i][j] = data[k][-1]
 					data.pop(k)
 					break
-	return Z, M, T
+	return Z, M, T, T2, T3
 
 
-Z, M, T = create_Z(X, Y, 10**-3, 1.0/6)
+Z, M, T, T2, T3 = create_Z(X, Y, 10**-3, 1.0/6)
 
 '''
 X = X-np.min(X)
@@ -133,6 +137,26 @@ ticks_labels3 = [create_string_tick(x) for x in ticks3]
 cbar3 = fig3.colorbar(im3, ticks=ticks3)
 cbar3.ax.set_yticklabels(ticks_labels3)
 
+fig4, ax4 = plt.subplots()
+im4 = ax4.imshow(T2, interpolation='bilinear', cmap='rainbow',
+               origin='lower', aspect='auto', extent=[np.min(X), np.max(X), np.min(Y), np.max(Y)]
+               ,vmin=np.min(T2), vmax=np.max(T2))
+#ax.scatter(X, Y)
+ticks4=np.linspace(np.min(T2), np.max(T2), 6)
+ticks_labels4 = [create_string_tick(x) for x in ticks4]
+cbar4 = fig4.colorbar(im4, ticks=ticks4)
+cbar4.ax.set_yticklabels(ticks_labels4)
+
+fig5, ax5 = plt.subplots()
+im5 = ax5.imshow(T3, interpolation='bilinear', cmap='rainbow',
+               origin='lower', aspect='auto', extent=[np.min(X), np.max(X), np.min(Y), np.max(Y)]
+               ,vmin=np.min(T3), vmax=np.max(T3))
+#ax.scatter(X, Y)
+ticks5=np.linspace(np.min(T3), np.max(T3), 6)
+ticks_labels5 = [create_string_tick(x) for x in ticks5]
+cbar5 = fig5.colorbar(im5, ticks=ticks5)
+cbar5.ax.set_yticklabels(ticks_labels5)
+
 
 ax.set_title("Temperature as a function of mass and b")
 ax.set_xlabel("mass", fontsize=16)
@@ -149,6 +173,18 @@ ax3.set_xlabel("mass", fontsize=16)
 ax3.set_ylabel("b", fontsize=16, rotation='horizontal')
 ax3.set_xscale('log')
 ax3.set_yscale('log')
+
+ax4.set_title("Time of transition to radiation dominance")
+ax4.set_xlabel("mass", fontsize=16)
+ax4.set_ylabel("b", fontsize=16, rotation='horizontal')
+ax4.set_xscale('log')
+ax4.set_yscale('log')
+
+ax5.set_title("Time of transition to matter dominance")
+ax5.set_xlabel("mass", fontsize=16)
+ax5.set_ylabel("b", fontsize=16, rotation='horizontal')
+ax5.set_xscale('log')
+ax5.set_yscale('log')
 
 
 
