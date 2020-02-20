3fcf97dff55984f08012eacececcd18b37f48873
Oskari Kerppo <oeoker@utu.fi>
2020-02-20 14:20:20 +0200

    New data set

--- reheating_scenario.py
+++ reheating_scenario.py
@@ -389,15 +389,15 @@ def generate_datapoints():
 	G_N = 1.0
 	plot = False
 
-	max_mass = -12
+	max_mass = -9
 	min_mass = -14
-	mass_points = np.logspace(min_mass, max_mass, 10, endpoint=True, base=10)
+	mass_points = np.logspace(min_mass, max_mass, 100, endpoint=True, base=10)
 
 	#lam = 10**min_mass*0.1
 
 	min_b = -1
 	max_b = 1
-	b_points = np.logspace(min_b, max_b, 10, endpoint=True, base=10)
+	b_points = np.logspace(min_b, max_b, 100, endpoint=True, base=10)
 
 	minimal_xi = 0.0
 	conformal_xi = 1.0/6
